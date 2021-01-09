00499134e50e2d116e88894523e5d83ee104e21b
rohan goel <rohan.goel84@gmail.com>
2021-01-09 01:03:01 -0500

    added more code

--- python3.py
+++ python3.py
@@ -7,5 +7,9 @@ list2 = [i for i in range(200)]
 
 print(list2)
 
-for i in zip(list1,list2):
-    print(i)
+#for i in zip(list1,list2):
+#    print(i)
+
+tup1 = tuple(zip(list1,list2))
+
+print(tup1)
